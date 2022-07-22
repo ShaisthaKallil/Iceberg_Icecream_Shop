547bf20795021cec8521647ccfbb35476649deec
Shaistha Kallil <kallilshai@gmail.com>
2022-07-21 21:01:44 -0400

    Added files and folders
    Added initial files and folders

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500&family=The+Nautigal:wght@700&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <!-- Style Sheet -->
+    <link rel ="stylesheet" href="css/styles.css">
+    <!--fontawesome-->
+   <script src="https://kit.fontawesome.com/1c9ad4b785.js" crossorigin="anonymous"></script>
+    <title>Iceberg Home Page</title>
+  </head>
+  <body>
+
+        <section id = "title">
+            <div class = "container-fluid">
+          <!-- Nav Bar -->
+          <nav class = "navbar navbar-expand-lg navbar-light" >
+            <a class = "navbar-brand" href = "#"><img src="images/logof.png" alt="ice-mockup"></a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+            <ul class="navbar-nav">
+              <li class="nav-item ms-auto">
+                <a class = "nav-link"href="#">MENU</a>
+              </li>
+              <li class="nav-item">
+                <a  class = "nav-link"href="#">DEALS</a>
+              </li>
+              <li class="nav-item">
+                <a  class = "nav-link"href="#">REVIEWS</a>
+              </li>
+              <li class="nav-item">
+                <a  class = "nav-link"href="#">CONTEST</a>
+              </li>
+              <li class="nav-item">
+                <a  class = "nav-link" href="#">CONTACT</a>
+              </li>
+            </ul>
+            </div>
+          </nav>
+
+          <!-- Header -->
+
+           <!-- Title -->
+
+
+          <div class = "row">
+            <div class = "col-lg-6" style="margin-top:150px;">
+              <h1>All The <span class = "head">Sweetness</span> <br> In One Place.</h1>
+              <br /><br />
+              <button type="button" class="btn btn-dark btn-lg"><i class="far fa-arrow-alt-circle-down"></i>&nbsp;Download</button>
+              <button type="button" class="btn btn-outline-light btn-lg" style="margin-left:20px;"><i class="fas fa-cart-plus"></i>&nbsp;Order Now</button>
+
+            </div>
+
+            <div class = "col-lg-6" style="overflow:hidden;">
+
+              <img class="icecream" src="images/icecream.png" alt="ice-mockup">
+            </div>
+          </div>
+        </div>
+        </section>
+
+
+        <!-- Footer -->
+
+        <footer id="footer">
+
+          <div>
+            <p>FAQ</p>
+           <p>Careers</p>
+           <p>Find store</p>
+
+           <p>Nutrition</p>
+           <p>Promotions</p>
+           <p>Privacy Policy</p>
+         </div>
+
+
+         <div>
+           <img src="images/iceberg-logo.png" alt="ice-mockup">
+           <p>Connect with us</p>
+           <img src="images/facebook.png" alt = "facebook">
+           <img src="images/instagram.png" alt = "instagram">
+           <img src="images/twitter.png" alt = "twitter">
+
+
+
+        </footer>
+         <!-- Optional JavaScript; choose one of the two! -->
+
+         <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
